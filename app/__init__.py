343ec3416976a7f349e3d1f6fe16464bb8f1b466
Rebecca Li <Rebecca.CYLi@gmail.com>
2021-08-02 18:14:00 -0400

    .env...?

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
+load_dotenv()
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
